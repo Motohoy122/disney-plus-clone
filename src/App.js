290e2db15e5295f666ef3cd63fe285a6867c90d6
Alex Hoyer <alexhoyer@Alexs-MBP.lan>
2021-11-13 22:21:30 -0800

    Added header component, created the structure and styling for links and logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
+import Header from './components/Header';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Header />
         <Routes>
           <Route path="/" element={<Login />} />
         </Routes>
