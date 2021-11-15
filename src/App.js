9dcc8043bf25450c8840ff2621f829dfd8a963fa
Alex Hoyer <alexhoyer@Alexs-MBP.lan>
2021-11-14 21:19:14 -0800

    added firebase log out functionality and added the Home component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
 import Header from './components/Header';
+import Home from './components/Home';
 import './App.css';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
         </Routes>
       </Router>
     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,43 @@
+import { useEffect } from "react";
 import styled from 'styled-components';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { auth, provider } from '../firebase';
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-import { selectUserName, selectUserPhoto, setUserLoginDetails } from '../features/user/userSlice';
+import { getAuth, signInWithPopup, signOut, GoogleAuthProvider } from "firebase/auth";
+import { selectUserName, selectUserPhoto, setSignOutState, setUserLoginDetails } from '../features/user/userSlice';
 
 const Header = (props) => {
     const dispatch = useDispatch();
     const history = useNavigate();
     const userName = useSelector(selectUserName);
     const userPhoto = useSelector(selectUserPhoto);
+
+    console.log(history)
+    useEffect(() => {
+        auth.onAuthStateChanged(async (user) => {
+            if(user) {
+                setUser(user);
+                history("/home");
+            }
+        })
+    }, [userName]);
+
     const handleAuth = () => {
-        signInWithPopup(auth, provider).then((result) => {
-            setUser(result.user);
-        }).catch((error) => {
-            alert(error.message);
-        });
+        if(!userName) {
+            signInWithPopup(auth, provider).then((result) => {
+                setUser(result.user);
+            }).catch((error) => {
+                alert(error.message);
+            });
+        } else if(userName) {
+            signOut(auth).then(() => {
+                dispatch(setSignOutState());
+                history('/');
+              }).catch((error) => {
+                alert(error.message);
+              });
+        }
+        
     }
 
     const setUser = (user) => {
@@ -27,6 +49,8 @@ const Header = (props) => {
             })
         );
     };
+
+
 
     return (
         <Nav>
@@ -67,7 +91,13 @@ const Header = (props) => {
                     </a>
                     
                 </NavMenu>
-                <UserImg src={userPhoto} alt={userName} />
+                <SignOut>
+                    <UserImg src={userPhoto} alt={userName} />
+                    <Dropdown>
+                        <span onClick={handleAuth}>Sign Out</span>
+                    </Dropdown>
+                </SignOut>
+                
                 </>
             }
         </Nav>
@@ -184,6 +214,45 @@ const Login = styled.a`
 
 const UserImg = styled.img`
     height: 100%;
+`;
+
+const Dropdown = styled.div`
+    position: absolute;
+    top: 48px;
+    right: opx;
+    background: rgb(19, 19, 19);
+    border: 1px solid rgba(151, 151, 151, 0.34);
+    border-radius: 4px;
+    box-shadow: rgb(0,0, 0 / 50%) 0px 0px 18px 0px;
+    padding: 10px;
+    font-size: 14px;
+    letter-spacing: 3px;
+    width: 100px;
+    opacity: 0;
+
+`;
+
+const SignOut = styled.a`
+    position: relative;
+    height: 48px;
+    width: 48px;
+    display: flex;
+    cursor: pointer;
+    align-items: center;
+    justify-content: center;
+
+    ${UserImg} {
+        border-radius: 50%;
+        width: 100%;
+        height: 100%;
+    }
+
+    &:hover {
+        ${Dropdown} {
+            opacity: 1;
+            transition-duration: 1s;
+        }
+    }
 `;
 
 export default Header;
